7403adcd38ef968048fd25da432c97c1cfa2e7c0
Wilma <kima_92@fastmail.com>
2020-06-08 10:34:20 -0400

    Optimized the intersection function, it now takes less time to finish running the Tests.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,3 +1,4 @@
+###########
 
 
 def intersection(arrays):
@@ -5,27 +6,23 @@ def intersection(arrays):
     result = []
 
     # For each array
-    for l in arrays:
+    for current_array in arrays:
         # Convert the array to dictionary
-        d = { i : 1 for i in l }
+        d = { i : 1 for i in current_array }
         # Append the dictionary to list_of_dicts = []
         list_of_dicts.append(d)
 
-    # While there's more than one dictionaryin list_of_dicts
-    while (len(list_of_dicts) > 1):
-
-        # Save the last dictionary in list_of_dicts,
-        # and remove it from list_of_dicts
-        last_dictinary = list_of_dicts.pop()
-
-        # For each key in the last dictionary
-        for key in last_dictinary.keys():
-            # Grab another dictionary from list_of_dicts
-            for another_dict in list_of_dicts:
-                # If this key is not in another_dict, and not in the result
-                if key in another_dict.keys() and key not in result:
-                    # Save it to the result
+        # If the length od list_of dicts is GREATER then 1
+        if len(list_of_dicts) > 1:
+            # For each key in current_array
+            for key in current_array:
+                # If the key is in the first dictionary in list_of_dicts,
+                # and it's not in result yet
+                if key in list_of_dicts[0] and key not in result:
+                    # Append it to result
                     result.append(key)
+            # Delete the first dictionary in list_of_dicts
+            del list_of_dicts[0]
 
     # Resturn the result
     return result
